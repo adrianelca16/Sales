0789f4b9afbc73ce4add9009c03a2c392476a78e
adrianelca16 <adrian.elca15@gmail.com>
2024-04-16 12:28:01 -0400

    Seed Db V1 finish

--- Api/Controllers/CountriesController.cs
+++ Api/Controllers/CountriesController.cs
@@ -36,18 +36,48 @@ namespace Api.Controllers
         [HttpPost]
         public async Task<ActionResult> PostAsync(Country country)
         {
-            _context.Add(country);
-            await _context.SaveChangesAsync();
-            return Ok(country);
+            try
+            {
+                _context.Add(country);
+                await _context.SaveChangesAsync();
+                return Ok(country);
+            }
+            catch (DbUpdateException dbUpdateException)
+            {
+                if (dbUpdateException.InnerException!.Message.Contains("duplica"))
+                {
+                    return BadRequest("Ya existe un pais con el mismo nombre");
+                }
+                return BadRequest(dbUpdateException.InnerException.Message);
+            }
+            catch (Exception exception)
+            {
+                return BadRequest(exception.Message);   
+            }
         }
 
 
         [HttpPut]
         public async Task<ActionResult> PutAsync(Country country)
         {
-            _context.Update(country);
-            await _context.SaveChangesAsync();
-            return Ok(country);
+            try
+            {
+                _context.Update(country);
+                await _context.SaveChangesAsync();
+                return Ok(country);
+            }
+            catch (DbUpdateException dbUpdateException)
+            {
+                if (dbUpdateException.InnerException!.Message.Contains("duplica"))
+                {
+                    return BadRequest("Ya existe un pais con el mismo nombre");
+                }
+                return BadRequest(dbUpdateException.InnerException.Message);
+            }
+            catch (Exception exception)
+            {
+                return BadRequest(exception.Message);
+            }
         }
 
 
